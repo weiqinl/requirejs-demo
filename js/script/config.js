289dc266f3c7e9090ed46e09bd05099233740c8a
刘伟勤 <wbliuwq@yonyou.com>
2017-07-26 17:17:38 +0800

    更新requirejs引入datatables.treegrid.js

--- js/script/config.js
+++ js/script/config.js
@@ -2,7 +2,8 @@ define(function() {
 	require.config({
 		baseUrl: './js/',
 		paths: {
-			jquery: 'lib/jquery-1.11.1.min'
+			"jquery": 'lib/jquery-1.11.1.min',
+			 "datatables.net": "lib/jquery.dataTables.min",
 		},
   urlArgs: "v=" + Math.random()
 	})
--- js/script/dataTablesTreeGrid.js
+++ js/script/dataTablesTreeGrid.js
@@ -1,5 +1,95 @@
 require(['config'], function() {
-	require(['jquery','lib/jquery.dataTables.min', 'lib/dataTables.treeGrid'], function($) {
-		$(console.log('加载中...'));
-	})
+    require(['jquery', 'datatables.net', 'lib/dataTables.treeGrid'], function($) {
+        $(console.log('加载中...'));
+        $(document).ready(function() {
+
+        	var columns = [
+                {
+                    title: '',
+                    target: 0,
+                    className: 'treegrid-control',
+                    data: function (item) {
+                        if (item.children) {
+                            return '<span>+<\/span>';
+                        }
+                        return '';
+                    }
+                },
+                {
+                    title: 'Name',
+                    target: 1,
+                    data: function (item) {
+                        return item.name;
+                    }
+                },
+                {
+                    title: 'Position',
+                    target: 2,
+                    data: function (item) {
+                        return item.position;
+                    }
+                },
+                {
+                    title: 'Office',
+                    target: 3,
+                    data: function (item) {
+                        return item.office;
+                    }
+                },
+                {
+                    title: 'Extn.',
+                    target: 4,
+                    data: function (item) {
+                        return item.extn;
+                    }
+                },
+                {
+                    title: 'Start date',
+                    target: 5,
+                    data: function (item) {
+                        return item.start;
+                    }
+                },
+                {
+                    title: 'Salary',
+                    target: 6,
+                    data: function (item) {
+                        return item.salary;
+                    }
+                }
+            ];
+            $('#example1').DataTable({
+                'columns': columns,
+                'ajax': 'js/script/arrays.txt',
+                'treeGrid': {
+                    'left': 10,
+                    'expandIcon': '<span>+<\/span>',
+                    'collapseIcon': '<span>-<\/span>'
+                }
+            });
+            // $('#exampleWithSelect').DataTable({
+            //     'select': {
+            //         'style': 'multi',
+            //         'selector': 'td:not(:first-child)'
+            //     },
+            //     'columns': columns,
+            //     'ajax': './arrays.txt',
+            //     'treeGrid': {
+            //         'left': 10,
+            //         'expandIcon': '<span>+<\/span>',
+            //         'collapseIcon': '<span>-<\/span>'
+            //     }
+            // });
+            // $('h4').on('click', function() {
+            //     var h4 = $(this);
+            //     if (h4.hasClass('show')) {
+            //         h4.removeClass('show').addClass('showed').html('-hide code');
+            //         h4.next().removeClass('hide');
+            //     } else {
+            //         h4.removeClass('showed').addClass('show').html('+show code');
+            //         h4.next().addClass('hide');
+            //     }
+            // });
+        });
+    })
 })
